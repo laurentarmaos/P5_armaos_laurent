2c333e419c08f7bb8b6b330342ce3f174f025d17
Laurent Armaos <laurent.armaos@gmail.com>
2022-07-20 11:34:04 +0300

    error correction in FireStationController

--- src/main/java/com/safetynet/safetynetalert/controller/FireStationController.java
+++ src/main/java/com/safetynet/safetynetalert/controller/FireStationController.java
@@ -32,7 +32,7 @@ public class FireStationController {
 		return service.createFireStation(dto);
 	}
 	
-	@GetMapping
+	@GetMapping("/get")
 	public @ResponseBody JSONObject getFireStation(@RequestParam String address) {
 		return service.getFireStation(address);
 	}
